365645cad944a6461f87a47cb954d335fccb35e3
PaulPinch <paul.pinch@yandex.ru>
2022-12-16 10:47:08 +0500

    Example 005 Condition If Else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите имя пользователя ");
+string  username = Console.ReadLine();
+if (username.ToLower() == "маша")
+{
+    System.Console.WriteLine("Ура! Это же Маша!");
+}
+else
+{
+    System.Console.Write("Привет, ");
+    System.Console.WriteLine(username);
+}
