fa7c9827f63b83c63f2972be9c5f6becfd372b2b
PaulPinch <paul.pinch@yandex.ru>
2022-12-18 11:00:32 +0500

    Method Find

---
+++ Example010_MethodFind/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+int[] array = {25, 36, 65, 87, 32, 98, 101, 74, 32};
+
+int n = array.Length;
+int find = 32;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        System.Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
